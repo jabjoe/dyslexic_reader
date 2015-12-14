710c1a9a0088df139a3645ba3ace3db27676445d
Joe Burmeister <joe.a.burmeister@gmail.com>
2015-12-14 16:30:50 +0000

    Add speech connection recovery on button click.

--- main.c
+++ main.c
@@ -9,12 +9,17 @@ extern void on_window_destroy_cb (GObject *object, gpointer user_data)
 
 extern void read_btn_clicked_cb (GObject *object, gpointer user_data)
 {
+    SPDConnection *speech_con = (SPDConnection*) g_object_get_data(G_OBJECT(user_data), "speech_con");
+
+    spd_say(speech_con, SPD_TEXT, "hello world");
 }
 
 
 extern void stop_btn_clicked_cb (GObject *object, gpointer user_data)
 {
+    SPDConnection *speech_con = (SPDConnection*) g_object_get_data(G_OBJECT(user_data), "speech_con");
 }
+
 
 
 int main(int argc, char* argv[])
@@ -28,19 +33,18 @@ int main(int argc, char* argv[])
         GtkBuilder              *builder;
         GtkWidget               *window;
 
-        spd_say(speech_con, SPD_TEXT, "hello world");
-
         builder = gtk_builder_new ();
         gtk_builder_add_from_file (builder, "ui.glade", NULL);
 
         window = GTK_WIDGET (gtk_builder_get_object (builder, "main_window"));
+
+        g_object_set_data(G_OBJECT(window), "speech_con", speech_con);
+
         gtk_builder_connect_signals (builder, NULL);          
         g_object_unref (G_OBJECT (builder));
 
         gtk_widget_show (window);
         gtk_main ();
-        
-        spd_say(speech_con, SPD_TEXT, "good bye world");
 
         spd_close(speech_con);
     }
